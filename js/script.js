a427f9835188fce5e1cd4da2d32ff54fe2d50f62
Slastenin Nikolay <slastenin13@gmail.com>
2021-08-27 22:06:24 +0300

    Написал скрипт калькулятора

--- js/script.js
+++ js/script.js
@@ -285,7 +285,7 @@ document.addEventListener(
     (event) => {
         let target = event.target;
         if (target.matches(".calc-item")) {
-            target.value = target.value.replace(/\D/g, "");
+            target.value = target.value.replace(/[^\d\.]/g, "");
         }
         if (target.matches(".form-name, .form-message, .mess")) {
             target.value = target.value.replace(/[^а-я\-\s]/gi, "");
@@ -321,3 +321,47 @@ document.addEventListener(
     },
     { capture: true }
 );
+
+//Calculator
+const calc = (price = 100) => {
+    const calcBlock = document.querySelector(".calc-block");
+    const calcType = document.querySelector(".calc-type");
+    console.log("calcType: ", calcType);
+    const calcSquare = document.querySelector(".calc-square");
+    const calcDay = document.querySelector(".calc-day");
+    const calcCount = document.querySelector(".calc-count");
+    const totalValue = document.getElementById("total");
+
+    const countSum = () => {
+        let total = 0;
+        let countValue = 1;
+        let dayValue = 1;
+        const typeValue = calcType.options[calcType.selectedIndex].value;
+        const squareValue = +calcSquare.value;
+
+        if (calcCount.value > 1) {
+            countValue += (calcCount.value - 1) / 10;
+        }
+
+        if (calcDay.value && calcDay.value < 5) {
+            dayValue *= 2;
+        } else if (calcDay.value && calcDay.value < 10) {
+            dayValue *= 1.5;
+        }
+
+        if (typeValue && squareValue) {
+            total = price * typeValue * squareValue * countValue * dayValue;
+        }
+
+        totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener("change", (event) => {
+        const target = event.target;
+        if (target.matches("select") || target.matches("input")) {
+            countSum();
+        }
+    });
+};
+
+calc(100);
